4c31245301837d055ef5f0c7876a63cc65b43ebc
aluno <etecae023@hotmail.com>
2025-06-23 17:54:26 -0300

    feat: comeca tela de cadastro de pets

--- src/app/pages/pets/pet-detalhes/pet-detalhes.page.ts
+++ src/app/pages/pets/pet-detalhes/pet-detalhes.page.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pet-detalhes',
   templateUrl: './pet-detalhes.page.html',
   styleUrls: ['./pet-detalhes.page.scss'],
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule
+  ],
   standalone: true,
 })
 export class PetDetalhesPage implements OnInit {
@@ -11,6 +19,7 @@ export class PetDetalhesPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
